e492e9b4f9bfb0df336a20c7a2010f1115d746bb
Kayllane  Farias <75839810+Aninimo@users.noreply.github.com>
2024-10-15 09:23:24 -0300

    Create somaImpar.js

---
+++ exercicios/somaImpar.js
@@ -0,0 +1,13 @@
+let num1 = prompt("Digite um número")
+let num2 = prompt("Digite outro número")
+let soma = 0
+
+for(let i = 0; i < num1.length; i++){
+  for(let j = 0; j < nums2.length; j++){
+    if(num1[i] < num1 && (num[i] / 2 % 0 == 1)){
+      soma += num[i]
+    }else if(num2[j] < num2 && (num[j] / 2 % 0 == 1)){
+      soma += num[j]
+    }
+  }
+}
